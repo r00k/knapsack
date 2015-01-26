638b31dfaba7c4c59d519bd25487b788e91a4679
Ben Orenstein <ben.orenstein@gmail.com>
2015-01-26 10:39:55 -0700

    Generate app

---
+++ project.clj
@@ -0,0 +1,9 @@
+(defproject knapsack "0.1.0-SNAPSHOT"
+  :description "A brute-force solver for the knapsack problem"
+  :url "http://xkcd.com/287/"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.6.0"]]
+  :main ^:skip-aot knapsack.core
+  :target-path "target/%s"
+  :profiles {:uberjar {:aot :all}})
