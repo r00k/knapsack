1e62ccd83c307009f114971450174ce03c179113
Ben Orenstein <ben.orenstein@gmail.com>
2015-01-26 11:49:11 -0700

    We started some stuff

--- src/knapsack/core.clj
+++ src/knapsack/core.clj
@@ -5,3 +5,44 @@
   "I don't do a whole lot ... yet."
   [& args]
   (println "Hello, World!"))
+
+; given a set of menu items, return all combinations of items
+; that add up to a given total (repeats of items is allowed)
+
+; algorithm:
+;   create a collection of single-item collections for each item
+;   until none of the collections can accept another item
+;     for each collection
+;       add each item
+
+(declare matches-target?)
+(declare generate-candidate-orders)
+
+(defn over-target? [menu target order]
+  (let [amounts (map menu order)
+        total (reduce + amounts)]
+    (> total target)))
+
+(defn no-orders-can-accept-additional-item?
+  [menu target orders order-generator]
+  (let [new-orders (order-generator menu orders)]
+    (every? (over-target? menu target new-orders))))
+
+(defn generate-initial-collections [menu]
+  (set (map vector (keys menu))))
+
+(defn solve-helper [menu target orders]
+  (loop [orders orders]
+    (if (no-orders-can-accept-additional-item? menu target orders)
+      (filter matches-target? orders)
+      (recur (generate-candidate-orders menu target orders)))))
+
+(defn solve [menu target]
+  ; generate initial collections
+  ; until none of the collections can accept another item
+  ;   for every order
+  ;     combine every item
+  ;   reject orders that are over target
+  ; filter orders that match target
+  (let [initial-collections (generate-initial-collections menu)]
+    (solve-helper menu target initial-collections)))
--- test/knapsack/core_test.clj
+++ test/knapsack/core_test.clj
@@ -2,6 +2,32 @@
   (:require [clojure.test :refer :all]
             [knapsack.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest end-to-end
+  (testing "A simple end-to-end example"
+    (let [menu {:soup 1, :steak 2}
+          target 3]
+      #_(is (= #{[:soup :soup :soup]
+                 [:steak :soup]}
+               (solve menu target))))))
+
+(deftest initial-collections
+  (is (= #{[:soup] [:steak]}
+         (generate-initial-collections {:soup 1, :steak 2}))))
+
+#_(deftest no-orders-can-accept-additional-item?-test
+  (let [menu {:soup 1, :steak 2}
+        target 3
+        orders #{[:soup :soup :soup]
+                 [:steak :soup]}
+        order-generator (fn [_ _] #{[:soup :soup :soup :soup]
+                                  [:soup :soup :soup :steak]
+                                  [:steak :soup :soup]
+                                  [:steak :soup :steak]})]
+    (is (= true
+           (no-orders-can-accept-additional-item? menu target orders order-generator)))))
+
+(deftest over-target-test
+  (is (= true
+         (over-target? {:steak 2} 3 [:steak :steak])))
+  (is (= false
+         (over-target? {:steak 2} 3 [:steak])))) 
